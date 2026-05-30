22f37a4cf307714bad5fb5377c5ebbe78ff15994
khubebanjare <info.khube@gmail.com>
2026-05-30 15:56:22 +0530

    dockerfile and docker-compose file are added and refactored the code.

--- src/main/java/org/khube/main/controller/CompanyController.java
+++ src/main/java/org/khube/main/controller/CompanyController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,6 +41,7 @@ public class CompanyController {
         }
     }
 
+    @GetMapping("/fetch/all")
     public ResponseEntity<List<CompanyDto>> fetchAllCompanies() {
         List<CompanyDto> companyList = companyService.fetchAllCompanies();
         return new ResponseEntity<>(companyList, HttpStatus.OK);
